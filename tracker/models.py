9d73067c332e158a771d84b60e37427251665b18
atc2146 <atc2146@columbia.edu>
2019-11-24 01:28:49 +0000

    add prelim model with some field only

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 # Create your models here.
+
+
+
+class Sighting(models.Model):
+    latitude = models.DecimalField(max_digits=20, decimal_places=20)
+    longitude = models.DecimalField(max_digits=20, decimal_places=20)
+
+    
+    AM = 'AM'
+    PM = 'PM'
+
+    SHIFT_CHOICES = (
+        (AM, 'AM'),
+        (PM, 'PM'),
+     )
+    
+    shift = models.CharField(
+            help_text=_('Time of sighting'),
+            max_length=2,
+            choices=SHIFT_CHOICES,
+            blank=True,
+            #null=True,
+            default='',
+            )
+
+    date = models.DateField('date sighted')
+    
+
+    def __str__(self):
+        return 'str defn'
